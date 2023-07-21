b12f93e62281bf36c2b119e74d1bd0bce47af7cb
Ephraimiyanda <iyandaephraim@gmail.com>
2023-07-21 15:12:12 +0100

    new commit

--- Route/user.route.js
+++ Route/user.route.js
@@ -1,38 +1,16 @@
-const express = require('express');
-const route = express.Router();
-const controller = require('../Controller/user.controller');
-const multer = require('multer');
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
-const cloudinary = require('cloudinary').v2;
-const postController = require('../Controller/postController')
-
-// Configure Cloudinary with your API credentials
-cloudinary.config({
-  cloud_name: 'dg0kdnwt1',
-  api_key: '743174149656362',
-  api_secret: 'NT0lp3G44g26b2jYH8BX5Ju0UsY',
-});
+const express = require('express')
+const route = express.Router()
+const controller = require('../Controller/user.controller')
 
 
-// Set up multer with Cloudinary storage
-const storage = new CloudinaryStorage({
-  cloudinary,
-  folder: 'uploads', // Folder in your Cloudinary account where the files will be stored
-  allowedFormats: ['jpg', 'png', 'jpeg', 'svg'], // Allowed file formats
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-    cb(null, file.fieldname + '-' + uniqueSuffix); // Set the filename for the uploaded file
-  },
-});
+route.get('/',(req,res)=>{
+    res.json('hello')
+})
 
-const upload = multer({ storage });
+route.post('/register',controller.registerUser);
+route.post('/login',controller.loginUser);
+route.post('/create', controller.upload.single('content'), controller.createPost);
 
-route.get('/', (req, res) => {
-  res.json('hello');
-});
 
-route.post('/register', controller.registerUser);
-route.post('/login', controller.loginUser);
-route.post('/create', upload.single('content'), postController.createPost);
 
-module.exports = route;
+module.exports = route
--- app.js
+++ app.js
@@ -6,23 +6,8 @@ app.use(CoRs())
 const port = process.env.PORT || 2000
 const body_parser = require('body-parser')
 const userRoute = require('./Route/user.route')
-const upload = require('./config/cloudinaryConfig');
 
-app.post('/upload', upload.single('file'), async (req, res) => {
-    try {
-      // The uploaded file details are available in req.file
-      const { originalname, mimetype, size, secure_url } = req.file;
-  
-      // Your code to handle the uploaded file goes here
-      // For example, you can save the file details to a database, send a response, etc.
-  
-      res.json({ message: 'File uploaded successfully', fileUrl: secure_url });
-    } catch (error) {
-      console.error('Error handling file upload:', error);
-      res.status(500).json({ success: false, error: 'Failed to handle file upload' });
-    }
-  });
-  
+
 app.listen(port,()=>{
     console.log(`app listening @ port ${port}`);
 })
