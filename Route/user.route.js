e77f32f6f3314ae505e8984d2ac171642511d6e5
Ephraimiyanda <iyandaephraim@gmail.com>
2023-07-22 15:52:29 -0500

    new commit

--- Route/user.route.js
+++ Route/user.route.js
@@ -1,15 +1,21 @@
 const express = require('express')
 const route = express.Router()
 const controller = require('../Controller/user.controller')
+const Multer = require("multer");
 
 
 route.get('/',(req,res)=>{
     res.json('hello')
 })
 
+const storage = new Multer.memoryStorage();
+const upload = Multer({
+  storage,
+});
+
 route.post('/register',controller.registerUser);
 route.post('/login',controller.loginUser);
 route.post('/create',controller.createPost);
-
+route.post('/upload', upload.single('image'),controller.createPost);
 
 module.exports = route
