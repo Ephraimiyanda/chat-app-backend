a24e877891210f09443bee0f464375850e7e58ed
Ephraimiyanda <iyandaephraim@gmail.com>
2023-07-22 14:45:15 -0500

    new commit

--- app.js
+++ app.js
@@ -13,32 +13,5 @@ route.get('/', (req, res) => {
 route.post('/register', controller.registerUser);
 route.post('/login', controller.loginUser);
 
-route.post('/create', upload.single('content'), async (req, res) => {
-  try {
-    const { sender, text } = req.body;
-
-    if (!req.file) {
-      return res.status(400).json({ error: 'Please upload an image' });
-    }
-
-    // Handle the image upload to Cloudinary
-    const imageUrl = await handleUploadFile(req.file);
-
-    // Create a new post document with the image URL
-    const newPost = {
-      sender,
-      text,
-      content: imageUrl,
-    };
-
-    // Call the createPost controller function to save the post to the database
-    const createdPost = await controller.createPost(newPost);
-
-    res.status(201).json({ success: true, post: createdPost });
-  } catch (error) {
-    console.error('Error creating post:', error);
-    res.status(500).json({ success: false, error: 'Failed to create the post' });
-  }
-});
-
+route.post('/create', upload.single('content'),controller.createPost);
 module.exports = route;
