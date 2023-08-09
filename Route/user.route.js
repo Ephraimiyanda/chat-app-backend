826e64b652e53d63a3063d81147f89e2ecf41932
Ephraimiyanda <iyandaephraim@gmail.com>
2023-08-09 17:58:01 +0100

    socket.io configuration ongoing

--- Route/user.route.js
+++ Route/user.route.js
@@ -1,20 +1,17 @@
 const express = require('express')
 const route = express.Router()
 const controller = require('../Controller/user.controller')
-const cors = require('cors');
 
-route.use(cors());
 route.get('/',(req,res)=>{
     res.json('hello')
 })
 
 
+
 route.post('/register',controller.registerUser);
 route.post('/login',controller.loginUser);
 route.post('/create',controller.createPost);
-route.get('/allUsers',controller.allUsers)
-route.get('/:userId', controller.getUserById);
-route.get('/messages/:userId', cors(), controller.getUserMessages);
-route.post('/messages/send', cors(), controller.sendMessage);
+route.get('allUsers',controller.allUsers)
+
 
 module.exports = route
