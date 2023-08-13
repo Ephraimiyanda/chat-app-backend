21117ec78cdc6018cc80440c777275c1953cd02d
Ephraimiyanda <iyandaephraim@gmail.com>
2023-08-13 18:17:06 +0100

    socket.io configuration ongoing

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const io = Socket(server);
 Db();
 
 const corsOptions = {
-  origin: 'http://localhost:3000'||"https://herosocial-chat-app.netlify.app",
+  origin: "*",
   methods: ['GET', 'POST'],
   credentials: true, // Allow cookies to be sent with the request
 };
