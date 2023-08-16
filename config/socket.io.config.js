0f61b8e32e5cfa7d39d8e50e085310457d77b3c9
Ephraimiyanda <iyandaephraim@gmail.com>
2023-08-16 20:19:08 +0100

    socket.io configuration ongoing

--- config/socket.io.config.js
+++ config/socket.io.config.js
@@ -25,7 +25,7 @@ function setupSocket(server) {
         // Emit the message to the sender
         socket.emit(`sender-${message.senderId}`, newMessage);
 
-        socket.to(`receiver-${message.receiverId}`, newMessage);
+        socket.to(message.receiverId).emit(`receiver-${message.receiverId}`, newMessage);
         
         
       } catch (error) {
